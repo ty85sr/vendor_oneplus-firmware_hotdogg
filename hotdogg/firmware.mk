cdd99efe8e545d0daa17fc9f151a2cac247583fb
ty85sr <ty85sr@gmail.com>
2024-01-25 15:26:31 -0600

    Initial upload
    Initial upload of OOS12 firmware for hotdogg

---
+++ hotdogg/firmware.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),hotdog)
+
+RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
+$(foreach f, $(notdir $(RADIO_FILES)), \
+    $(call add-radio-file,radio/$(f)))
+
+FIRMWARE_IMAGES := \
+    LOGO \
+    abl \
+    aop \
+    bluetooth \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    multiimgoem \
+    oem_stanvbk \
+    opproduct \
+    qupfw \
+    storsec \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
+
+AB_OTA_PARTITIONS += $(FIRMWARE_IMAGES)
+
+endif    
+
